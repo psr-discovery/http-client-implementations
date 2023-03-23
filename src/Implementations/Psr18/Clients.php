e62448fe81f869081aeed8378b8723437491d2f9
Evan Sims <hello@evansims.com>
2023-03-23 16:58:05 -0500

    Update Clients.php

--- src/Implementations/Psr18/Clients.php
+++ src/Implementations/Psr18/Clients.php
@@ -34,55 +34,56 @@ final class Clients extends Implementation implements ClientsContract
 
         self::$candidates = CandidatesCollection::create();
 
-        // psr-mock/http-client-implementation 1.0+ is PSR-18 compatible.
         self::$candidates->add(CandidateEntity::create(
             package: 'psr-mock/http-client-implementation',
             version: '^1.0',
             builder: static fn (string $class = '\PsrMock\Psr18\Client'): object => new $class(),
         ));
 
-        // guzzlehttp/guzzle 7.0+ is PSR-18 compatible.
+        self::$candidates->add(CandidateEntity::create(
+            package: 'php-http/mock-client',
+            version: '^1.5',
+            builder: static fn (string $class = '\Http\Mock\Client'): object => new $class(
+                responseFactory: Discover::httpResponseFactory(),
+            ),
+        ));
+
         self::$candidates->add(CandidateEntity::create(
             package: 'guzzlehttp/guzzle',
             version: '^7.0',
             builder: static fn (string $class = '\GuzzleHttp\Client'): object => new $class(),
         ));
 
-        // symfony/http-client 4.3+ is PSR-18 compatible.
         self::$candidates->add(CandidateEntity::create(
             package: 'symfony/http-client',
-            version: '^4.3',
+            version: '^4.3 || ^5.0 || ^6.0',
             builder: static fn (string $class = '\Symfony\Component\HttpClient\Psr18Client'): object => new $class(
                 responseFactory: Discover::httpResponseFactory(),
                 streamFactory: Discover::httpStreamFactory(),
             ),
         ));
 
-        // php-http/guzzle6-adapter 2.0+ is PSR-18 compatible.
         self::$candidates->add(CandidateEntity::create(
             package: 'php-http/guzzle6-adapter',
             version: '^2.0',
             builder: static fn (string $class = '\Http\Adapter\Guzzle6\Client'): object => new $class(),
         ));
 
-        // php-http/guzzle7-adapter 1.0+ is PSR-18 compatible.
         self::$candidates->add(CandidateEntity::create(
             package: 'php-http/guzzle7-adapter',
-            version: '^1.0',
+            version: '^0.1',
             builder: static fn (string $class = '\Http\Adapter\Guzzle7\Client'): object => new $class(),
         ));
 
-        // php-http/curl-client 2.0+ is PSR-18 compatible.
         self::$candidates->add(CandidateEntity::create(
             package: 'php-http/curl-client',
-            version: '^2.0',
+            version: '^2.1',
             builder: static fn (string $class = '\Http\Client\Curl\Client'): object => new $class(
                 responseFactory: Discover::httpResponseFactory(),
                 streamFactory: Discover::httpStreamFactory(),
             ),
         ));
 
-        // kriswallsmith/buzz 1.0+ is PSR-18 compatible.
         self::$candidates->add(CandidateEntity::create(
             package: 'kriswallsmith/buzz',
             version: '^1.0',
@@ -91,7 +92,6 @@ final class Clients extends Implementation implements ClientsContract
             ),
         ));
 
-        // php-http/socket-client 2.0+ is PSR-18 compatible.
         self::$candidates->add(CandidateEntity::create(
             package: 'php-http/socket-client',
             version: '^2.0',
@@ -100,7 +100,6 @@ final class Clients extends Implementation implements ClientsContract
             ),
         ));
 
-        // php-http/guzzle5-adapter 2.0+ is PSR-18 compatible.
         self::$candidates->add(CandidateEntity::create(
             package: 'php-http/guzzle5-adapter',
             version: '^2.0',
@@ -109,10 +108,9 @@ final class Clients extends Implementation implements ClientsContract
             ),
         ));
 
-        // voku/httpful 0.2.20+ is PSR-18 compatible.
         self::$candidates->add(CandidateEntity::create(
             package: 'voku/httpful',
-            version: '^0.2.20',
+            version: '^2.2',
             builder: static fn (string $class = '\Httpful\Client'): object => new $class(),
         ));
 
